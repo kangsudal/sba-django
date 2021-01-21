d9267f66eda192bfa09a2dc0c51e2c766d3ed853
kangsudal-ncloudserver <kangsudal@gmail.com>
2021-01-21 17:27:05 +0900

    practice path counter

--- novelApp/urls.py
+++ novelApp/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns=[
-    path('',views.introPage),
+    path('', views.post_list),
+    path('<str:character1>/<str:character2>/',views.introPage),
 ]
--- novelApp/views.py
+++ novelApp/views.py
@@ -3,5 +3,8 @@ from django.shortcuts import render
 # from django.http.response import HttpResponse
 
 # Create your views here.
-def introPage(req,):
-    return render(req,'introPage.html')
+def post_list(req):
+    return
+def introPage(req,character1,character2):
+    context = {"name1":character1,"name2":character2}
+    return render(req,'introPage.html',context)
